8bda36ac301e06c2a67b86dfaa990b5c0b576582
Caio Contursi <127901007+caiocontursi@users.noreply.github.com>
2023-03-19 12:50:11 -0300

    My first project

---
+++ index.js
@@ -0,0 +1,17 @@
+
+
+let firstName = prompt('Favor digitar seu nome: ')
+document.getElementById('nome').innerHTML = firstName
+
+function carConteudo(){
+    document.getElementById("conteudo").innerHTML = "<p>Obrigado pela avaliação!</p>";
+}
+
+const url = 'https://www.instagram.com/humoresportivo.oficial/?hl=pt-br'
+const button = document.querySelector
+
+function openInNewTab(url) {
+    const win = window.open(url, 'blank')
+    win.focus()
+}
+
